a0ff56a20ec3f93e21b687e867725832d9b5f861
0000Tetiana-Kotolup0000 <kotolup24@gmail.com>
2022-08-11 00:13:14 +0200

    napojeni jQuery uz funguje)

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,27 @@
-var button = document.querySelector('#button');
-var button = document.querySelector('#modal');
-var button = document.querySelector('#close');
+// var button = document.querySelector('#button');
+// var button = document.querySelector('#modal');
+// var button = document.querySelector('#close');
 
 
-button.addEventListener('click', function(){
-    modal.classList.add('modal_active');
-});
+// button.addEventListener('click', function(){
+//     modal.classList.add('modal_active');
+// });
 
-close.addEventListener('click', function(){
-    modal.classList.removel('modal_active');
+// close.addEventListener('click', function(){
+//     modal.classList.removel('modal_active');
+// });
+
+
+$(document).ready(function(){
+    var button = $('#button');
+    var modal = $('#modal');
+    var close = $('#close');
+
+    button.on('click', function(){
+        modal.addClass('modal_active');
+    });
+
+    close.on('click', function(){
+        modal.removeClass('modal_active');
+    });
 });
